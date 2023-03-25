0e3d0e1d9ed2d57d326cd5a5f5edadfbee2fbe03
Etienne de Martel <edemartel@gmail.com>
2023-03-25 17:34:53 -0400

    Don't version sources, download them with a script.
    That means data generation is no longer done at build time.

---
+++ source_data/download_data.ps1
@@ -0,0 +1,13 @@
+$data = Get-Content -Path "$PSScriptRoot\sources.jsonc" -Raw | ConvertFrom-Json -AsHashtable
+
+foreach ($pair in $data.GetEnumerator()) {
+    $dir = "$PSScriptRoot\$($pair.Key)"
+    New-Item -Path $dir -ItemType Directory -ErrorAction Ignore
+    foreach ($str in $pair.Value) {
+        $url = [uri]$str
+        $dest = "$dir\$($url.Segments[-1])"
+        if (!(Test-Path -Path $dest -PathType Leaf)) {
+            Invoke-WebRequest -Uri $url -OutFile $dest
+        }
+    }
+}
