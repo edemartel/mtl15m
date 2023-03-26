6ea550b858253bee42cb298d339713d6909d3f0e
Etienne de Martel <edemartel@gmail.com>
2023-03-25 23:53:29 -0400

    Cross platform path separators.

--- data/source/download_data.ps1
+++ data/source/download_data.ps1
@@ -1,11 +1,11 @@
-$data = Get-Content -Path "$PSScriptRoot\sources.jsonc" -Raw | ConvertFrom-Json -AsHashtable
+$data = Get-Content -Path "$PSScriptRoot/sources.jsonc" -Raw | ConvertFrom-Json -AsHashtable
 
 foreach ($pair in $data.GetEnumerator()) {
-    $dir = "$PSScriptRoot\$($pair.Key)"
+    $dir = "$PSScriptRoot/$($pair.Key)"
     New-Item -Path $dir -ItemType Directory -ErrorAction Ignore
     foreach ($str in $pair.Value) {
         $url = [uri]$str
-        $dest = "$dir\$($url.Segments[-1])"
+        $dest = "$dir/$($url.Segments[-1])"
         if (!(Test-Path -Path $dest -PathType Leaf)) {
             Invoke-WebRequest -Uri $url -OutFile $dest
         }
