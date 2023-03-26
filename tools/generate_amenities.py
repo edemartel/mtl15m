1c385c178127ef60b5200dfade76a364366fff50
Etienne de Martel <edemartel@gmail.com>
2023-03-26 15:31:14 -0400

    Turns out parks were broken. Woops.

--- tools/generate_amenities.py
+++ tools/generate_amenities.py
@@ -3,6 +3,7 @@ from data import source_path, generated_path
 import csv
 import json
 import shapefile
+import geojson
 from shapely import geometry
 from shapely import ops
 import pyproj
@@ -32,9 +33,9 @@ with open(os.path.join(source_path, 'amenities', 'businesses.csv'), 'r', encodin
 with open(os.path.join(source_path, 'amenities', 'occupation-commerciale-2022.csv'), 'r', encoding='utf-8', newline='') as data_file:
     reader = csv.reader(data_file)
     next(reader, None)
-    
+
     for row in reader:
-        _,_,_,_,_,_,_,_,_,_,_,_,_,_,_,ID_USAGE1,_,ID_USAGE2,_,ID_USAGE3,_,_,_,_,_,VACANT_A_LOUER,_,_,_,_,_,_,_,_,_,_,_,_,_,LAT,LONG = row
+        _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, ID_USAGE1, _, ID_USAGE2, _, ID_USAGE3, _, _, _, _, _, VACANT_A_LOUER, _, _, _, _, _, _, _, _, _, _, _, _, _, LAT, LONG = row
         if VACANT_A_LOUER.lower() != 'non':
             continue
 
@@ -58,35 +59,34 @@ with open(os.path.join(source_path, 'amenities', 'occupation-commerciale-2022.cs
 with open(os.path.join(source_path, 'amenities', 'lieuxculturels.csv'), 'r', encoding='utf-8', newline='') as data_file:
     reader = csv.reader(data_file)
     next(reader, None)
-    
+
     for row in reader:
-        _,NomReseau,_,_,_,_,_,_,_,Longitude,Latitude,_ = row
-        
+        _, NomReseau, _, _, _, _, _, _, _, Longitude, Latitude, _ = row
+
         if NomReseau.lower() == 'bibliothèque':
             pt = [float(Longitude), float(Latitude)]
-            amenities.setdefault('library', []).append(pt)           
+            amenities.setdefault('library', []).append(pt)
 
-with open(os.path.join(source_path, 'amenities', 'espace_vert.csv'), 'r', encoding='utf-8', newline='') as data_file:
-    reader = csv.reader(data_file)
-    next(reader, None)
-    
-    for row in reader:
-        OBJECTID,Type,Lien,Nom,NUM_INDEX,SUPERFICIE,PROPRIETE,GESTION,COMPETENCE,TYPO1,TYPO2 = row
-        
-        if TYPO1.lower().startswith('parc'):
-            pt = [float(LONG), float(LAT)]
-            amenities.setdefault('park', []).append(pt)
+with open(os.path.join(source_path, 'amenities', 'espace_vert.json'), 'r', encoding='utf-8', newline='') as data_file:
+    parks: geojson.FeatureCollection = geojson.load(data_file)
+
+    for feature in parks.features:
+        park_type = feature.properties['TYPO1']
+        if park_type and park_type.lower().startswith('parc'):
+            polygon = geometry.shape(feature.geometry)
+            # TODO: might be better to instead consider park boundaries
+            amenities.setdefault('park', []).append([polygon.centroid.x, polygon.centroid.y])
 
 
 with shapefile.Reader(os.path.join(source_path, 'amenities', 'etablissements-meq-mes-esrishp.zip/PPS_Public_Ecole.shp'), encoding='latin1') as reader:
-     for shape_rec in reader.iterShapeRecords():
+    for shape_rec in reader.iterShapeRecords():
         level = shape_rec.record.ORDRE_ENS.lower()
         if level != 'primaire' and level != 'secondaire':
             continue
         cp = shape_rec.record.CD_POSTL_I
         if cp[0] != 'H' or cp[0:2] == 'H0' or cp[0:2] == 'H7':
             continue
-        
+
         pt: geometry.Point = geometry.shape(shape_rec.shape.__geo_interface__)
         match level:
             case 'primaire':
@@ -102,15 +102,15 @@ project = pyproj.Transformer.from_crs(
 
 known_stops = set()
 with shapefile.Reader(os.path.join(source_path, 'amenities', 'stm_sig.zip/stm_arrets_sig.shp'), encoding='latin1') as reader:
-     for shape_rec in reader.iterShapeRecords():
+    for shape_rec in reader.iterShapeRecords():
         if not shape_rec.record.route_id:
-            continue # not real stops
+            continue  # not real stops
         if not any(int(x) in [1, 2, 5] for x in shape_rec.record.route_id.split(',')):
-            continue # only keep green, orange and blue metro lines
+            continue  # only keep green, orange and blue metro lines
         if shape_rec.record.stop_code in [10282, 10286, 10288]:
-            continue # remove Laval orange line stations
+            continue  # remove Laval orange line stations
         if shape_rec.record.stop_code in known_stops:
-            continue # skip duplicates
+            continue  # skip duplicates
         known_stops.add(shape_rec.record.stop_code)
 
         pt: geometry.Point = geometry.shape(shape_rec.shape.__geo_interface__)
