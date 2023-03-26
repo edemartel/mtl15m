1e7a1d5ba9676720e01f0f5dc0a382dd172e3ce1
Etienne de Martel <edemartel@gmail.com>
2023-03-26 19:39:55 -0400

    Make sure we use the right coordinate system.

--- tools/finalise_map.py
+++ tools/finalise_map.py
@@ -6,36 +6,51 @@ import geojson.geometry
 from shapely import geometry
 from shapely import ops
 import pyproj
+import math
 from data import generated_path, public_path
 
+
 MAX_DISTANCE = 2500
-
-def get_real_distance(center: geometry.Point, service: geometry.Point):
-    # TODO: use OpenRouteService
-    return center.distance(service)
-
-with open(os.path.join(generated_path, 'map.json'), 'r', encoding='utf-8') as input_file:
-    base_map: geojson.FeatureCollection = geojson.load(input_file)
 
 from_proj = pyproj.CRS('EPSG:4326')
 to_proj = pyproj.CRS('EPSG:3857')
 project = pyproj.Transformer.from_crs(
     from_proj, to_proj, always_xy=True).transform
 
+class MapPoint:
+    point_in_degrees: geometry.Point
+    point_in_metres: geometry.Point
+
+    def __init__(self, pt: geometry.Point):
+        self.point_in_degrees = pt
+        self.point_in_metres = ops.transform(project, pt)
+
+def get_real_distance(center: MapPoint, service: MapPoint):
+    # TODO: use OpenRouteService
+    return center.point_in_metres.distance(service.point_in_metres)
+
+with open(os.path.join(generated_path, 'map.json'), 'r', encoding='utf-8') as input_file:
+    base_map: geojson.FeatureCollection = geojson.load(input_file)
+
 amenities = {}
 for file_name in os.listdir(os.path.join(generated_path, 'amenities')):
     type = os.path.splitext(file_name)[0]
     with open(os.path.join(generated_path, 'amenities', file_name), 'r', encoding='utf-8') as input_file:
-        amenities[type] = [ops.transform(project, geometry.Point(pt)) for pt in json.load(input_file)]
+        amenities[type] = [MapPoint(geometry.Point(pt)) for pt in json.load(input_file)]
 
 for feature in base_map.features:
-    center: geometry.Point = ops.transform(project, geometry.shape(feature.properties['center']))
+    center = MapPoint(geometry.shape(feature.properties['center']))
     distances = {}
     for type, points in amenities.items():
-        filtered = filter(lambda pt: center.distance(pt) <= MAX_DISTANCE, points)
-        best = min(filtered, default=None, key=lambda pt: get_real_distance(center, pt))
-        if best is not None:
-            distances[type] = center.distance(best)
+        best = (None, math.inf)
+        for pt in points:
+            if center.point_in_metres.distance(pt.point_in_metres) > MAX_DISTANCE:
+                continue
+            dist = get_real_distance(center, pt)
+            if dist < best[1]:
+                best = (pt, dist)
+        if best[0] is not None:
+            distances[type] = best[1]
     feature.properties['distances'] = distances
 
 
