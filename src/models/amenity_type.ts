c6d8ed3e08d037410ba47514cadd28ab212f4460
Etienne de Martel <edemartel@gmail.com>
2023-03-25 22:23:36 -0400

    List of amenity types.

---
+++ src/models/amenity_type.ts
@@ -0,0 +1,13 @@
+export enum AmenityType {
+    Clinic = 'clinic',
+    Daycare = 'daycare',
+    FoodStore = 'food_store',
+    Library = 'library',
+    MetroStation = 'metro_station',
+    Park = 'park',
+    Pharmacy = 'pharmacy',
+    PrimarySchool = 'primary_school',
+    Restaurant = 'restaurant',
+    SecondarySchool= 'secondary_school',
+    SwimmingPool= 'swimming_pool',
+}
